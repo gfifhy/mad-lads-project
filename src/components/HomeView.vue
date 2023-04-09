4ceaaaac976780d511588a049157da13b17a5acb
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-09 20:55:14 +0800

    initial commit

---
+++ src/components/HomeView.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div data-scroll-container>
+    <section data-scroll-section>
+      <h1>Scroll Smooth Testing</h1>
+      <p>Ok nice</p>
+    </section>
+    <section class="section2" id="my-section" data-scroll-section>
+      <h1>TESTING</h1>
+      <p>Ok nice</p>
+      <!--      <h2 data-scroll data-scroll-speed="1">What's up?</h2>
+            <p data-scroll data-scroll-speed="2">😬</p>-->
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import LocomotiveScroll from "locomotive-scroll";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  mounted() {
+    new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+    });
   },
 };
 </script>
+
+<style scoped>
+@import "@/assets/css/base.css";
+
+* {
+  box-sizing: border-box;
+}
+
+section {
+  height: 100vh;
+  padding: 10em;
+}
+</style>
